6428b0fd489d0615b83099d9abd4602640da9125
newbieJanghan <102276240+newbieJanghan@users.noreply.github.com>
2022-06-08 14:50:56 +0900

    Update filtered_average(with_lambda).py
    code modify

--- filtered_average(with_lambda).py
+++ filtered_average(with_lambda).py
@@ -1,14 +1,12 @@
 import sys
 from functools import reduce
 
-N = int(input())
-
-def filtered_average(lst, N):
-    average = reduce(lambda x, y: x + y, lst) / N
+def filtered_average(lst):
+    average = reduce(lambda x, y: x + y, lst) / len(lst)
     filtered = list(filter(lambda x: x >= average, lst))
     return reduce(lambda x, y: x + y, filtered) / len(filtered)
 
 
-average = filtered_average(list(map(int, sys.stdin.readline().split())), N)
+average = filtered_average(list(map(int, sys.stdin.readline().split())))
 
 print(int(average))
