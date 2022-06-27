b43c320d9085f2e0fa5a3833def50cfc6d83ca3c
Janghan Bae <102276240+newbieJanghan@users.noreply.github.com>
2022-06-27 14:03:24 +0900

    Update dial key pad.js

--- dial key pad.js
+++ dial key pad.js
@@ -63,3 +63,44 @@ function solution(numbers, hand) {
 // 반성
 // 1. 출제 의도를 잘 모르겠음. other solution을 보니 인접리스트, 그리드 등 여러 자료구조를 활용했던데, 난 모르겠기도 하거니와 굳이 싶었음.
 // 2. 코드를 단순화해보자. 거리 계산하는 함수 생성하고 중복되는 조건문을 제거해보자.
+
+
+function getDist(num, key) {
+    if ( num == 0 ) {
+        return (key == 2 || key == 5 || key == 8 || key == 11) 
+                    ? Math.abs(11-key) / 3 
+                    : Math.abs(15-key) / 3
+    } else {
+        return (key == 2 || key == 5 || key == 8 || key == 11) 
+                    ? Math.abs(num-key) / 3 
+                    : parseInt((Math.abs(num-key)+1) / 3) + 1   
+    }
+}
+
+function solution(numbers, hand) {
+    let l = 12;
+    let r = 12;
+    let answer = ''
+    numbers.map(el => {
+        if ( el == 1 || el == 4 || el == 7) {
+            answer += "L"
+            l = el + 2
+        } else if ( el == 3 || el == 6 || el == 9) {
+            answer += "R"
+            r = el
+        } else {
+            const l_dist = getDist(el, l)
+            const r_dist = getDist(el, r)
+            if (hand == "left") {
+                l_dist <= r_dist
+                    ? (answer += "L", l = el == 0 ? 11 : el)
+                    : (answer += "R", r = el == 0 ? 11 : el)
+            } else {
+                r_dist > l_dist 
+                    ? (answer += "L", l = el == 0 ? 11 : el)
+                    : (answer += "R", r = el == 0 ? 11 : el)
+            }
+        } 
+    })
+    return answer
+}
